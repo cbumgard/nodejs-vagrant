0efc2061548c61903258c11223db5297daade1be
Chris Bumgardner <cbumgard@gmail.com>
2014-03-10 22:39:14 -0700

    packages working cbumgard/nodejs box

--- package.sh
+++ package.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-vagrant package --output nodejs.box
-#vagrant package --vagrantfile ./Vagrantfile --include ./sh/core.sh,./sh/heroku.sh,./sh/nodejs.sh,./sh/redis.sh,./sh/travis.sh,./sh/github.sh,./sh/mongodb.sh,./sh/provision.sh,./sh/repair-mac.sh,./sh/vim.sh --output nodejs.box
+vagrant package --vagrantfile ./box/Vagrantfile --output nodejs.box
